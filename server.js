94640ccadd03f8de37aff3a986ef0fc4b30b73d9
itzdeepanshu30 <deepanshusinghal509@gmail.com>
2026-06-10 01:23:11 +0530

    Connect database

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 
+const connectDB = require("./config/db");
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 

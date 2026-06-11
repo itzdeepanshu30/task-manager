3a16dbb7340f6dc26871f114f9ba499cf9dfa3a1
itzdeepanshu30 <deepanshusinghal509@gmail.com>
2026-06-11 16:28:22 +0530

    Implement create and read Api

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
+const Task = require("./models/Task");
 require("dotenv").config();
 
 const express = require("express");
 
 const connectDB = require("./config/db");
 const app = express();
-
-connectDB();
-
 app.use(express.json());
 
-app.get("/tasks", (req, res) => {
-  res.send("Get all tasks");
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find();
+
+        res.status(200).json(tasks);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 });
 
-app.post("/tasks", (req, res) => {
-  console.log(req.body);
+app.post("/tasks", async (req, res) => {
+    try {
+        const task = await Task.create({
+            title: req.body.title
+        });
 
-  res.send("Task created");
+        res.status(201).json(task);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 });
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+    app.listen(5000, () => {
+        console.log("Server running on port 5000");
+    });
 });
